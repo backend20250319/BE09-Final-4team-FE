e498b65fbcee7a6094bab8d830370f74852a0968
hyeblee <hyeb1224@naver.com>
2025-08-11 12:40:05 +0900

    refactor: 머지 충돌 해결

--- app/documents/page.jsx
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-import DocumentsTable from './DocumentsTable';
-
-export default function DocumentsPage() {
-  const dir = path.join(process.cwd(), 'app/documents/files');
-  const files = fs.readdirSync(dir);
-  return <DocumentsTable files={files} />;
-} 
