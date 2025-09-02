f1b8d07cb6f81bdcf54142fed3b3374bf2dd39d3
Seokjin Lee <seokjin.dev@gmail.com>
2025-09-02 12:44:03 +0900

    index.ts에 불필요한 export 제거

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -3,24 +3,3 @@ export * from './common';
 export { approvalApi } from './approval';
 export { userApi } from './user';
 export { organizationApi } from './organization';
-
-export type { ApiResult, PageResult, Pageable } from './common';
-export type { 
-  DocumentStatus, 
-  TargetType, 
-  FieldType, 
-  UserRole, 
-  ActivityType 
-} from './approval';
-export type { 
-  UserResponseDto, 
-  UserCreateDto, 
-  UserUpdateDto,
-  TokenResponseDto,
-  LoginRequestDto 
-} from './user';
-export type { 
-  OrganizationDto, 
-  CreateOrganizationRequest,
-  EmployeeAssignmentDto 
-} from './organization';
